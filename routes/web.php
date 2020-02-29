91ae4046ebef4a3dee5696887bc7dfe002cec486
vagrant <vagrant@homestead>
2020-02-29 16:56:34 +0000

    邮箱认证

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,4 @@ Route::post('password/reset','Auth\ResetPasswordController@reset')->name('passwo
 // Email 认证相关路由
 Route::get('email/verify','Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}','Auth\VerificationController@verify')->name('verification.verify');
-Route::post('email/resend','AuthVerificationController@resend')->name('verification.resend');
+Route::post('email/resend','Auth\VerificationController@resend')->name('verification.resend');
