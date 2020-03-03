e3f578fad2e47a069b7722c66402264b78095b6a
vagrant <vagrant@homestead>
2020-03-03 03:31:41 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,5 @@ Route::post('email/resend','Auth\VerificationController@resend')->name('verifica
 // 个人中心
 Route::resource('users','UsersController',['only' => ['show', 'update', 'edit']]);
 
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
