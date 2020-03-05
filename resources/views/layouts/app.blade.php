d1aa7c050c4d41bd350c6c4a35e0e6d1f5f2484e
vagrant <vagrant@homestead>
2020-03-05 09:55:53 +0000

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    @yield('styles')
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page">
@@ -26,5 +27,6 @@
 
     <!--Scripts-->
     <script src="{{ mix('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
