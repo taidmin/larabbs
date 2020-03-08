84da095e4ef59e8e2bad2ec23c9aa4cff28785a0
vagrant <vagrant@homestead>
2020-03-08 11:54:15 +0000

    回复数据

--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,4 @@ Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 // 话题图片上传
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
