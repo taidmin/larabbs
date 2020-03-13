aec6a7ecd3723b5b0f9497dbe361e16562cc98a0
vagrant <vagrant@homestead>
2020-03-13 18:12:52 +0000

    站点权限部署

--- database/migrations/2020_03_11_225247_seed_roles_and_permissions_data.php
+++ database/migrations/2020_03_11_225247_seed_roles_and_permissions_data.php
@@ -1,12 +1,10 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\DB;
 
 class SeedRolesAndPermissionsData extends Migration
 {
@@ -17,8 +15,8 @@ class SeedRolesAndPermissionsData extends Migration
      */
     public function up()
     {
-        // 需清楚缓存，否则会报错
-        app(\Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
+        // 需清除缓存，否则会报错
+        app(Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
 
         // 先创建权限
         Permission::create(['name' => 'manage_contents']);
@@ -26,13 +24,13 @@ class SeedRolesAndPermissionsData extends Migration
         Permission::create(['name' => 'edit_settings']);
 
         // 创建站长角色，并赋予权限
-        $founder = Role::create(['name' => 'founder']);
+        $founder = Role::create(['name' => 'Founder']);
         $founder->givePermissionTo('manage_contents');
         $founder->givePermissionTo('manage_users');
         $founder->givePermissionTo('edit_settings');
 
         // 创建管理员角色，并赋予权限
-        $maintainer = Role::create(['name' => 'maintainer']);
+        $maintainer = Role::create(['name' => 'Maintainer']);
         $maintainer->givePermissionTo('manage_contents');
     }
 
@@ -44,7 +42,7 @@ class SeedRolesAndPermissionsData extends Migration
     public function down()
     {
         // 需清除缓存，否则会报错
-        app(\Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
+        app(Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
 
         // 清空所有数据表数据
         $tableNames = config('permission.table_names');
