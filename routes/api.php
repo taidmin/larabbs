68a54cb57965adc24e4f0b65ffe2c3f6f02b6f0f
vagrant <vagrant@homestead>
2020-03-21 08:24:14 +0000

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -13,12 +13,23 @@
 
 use Illuminate\Http\Request;
 
-Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function (){
-    // 短信验证码
-    Route::post('verificationCodes','VerificationCodesController@store')
-        ->name('verificationCodes.store');
+Route::prefix('v1')->namespace('Api')
+                         ->name('api.v1.')
+                         ->group(function (){
 
-    // 用户注册
-    Route::post('users', 'UsersController@store')
-        ->name('users.store');
+         // 登录相关的
+         Route::middleware('throttle:'. config('api.rate_limits.sign'))->group(function(){
+             // 短信验证码
+             Route::post('verificationCodes','VerificationCodesController@store')
+                 ->name('verificationCodes.store');
+
+             // 用户注册
+             Route::post('users', 'UsersController@store')
+                 ->name('users.store');
+         });
+
+         Route::middleware('throttle:'. config('api.rate_limits.access'))->group(function(){
+
+         });
+
 });
