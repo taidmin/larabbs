ed6d8865efa1aa52451a7f08f267ecdfe13f4131
vagrant <vagrant@homestead>
2020-03-22 10:21:14 +0000

    微信登录

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
      * @var array
      */
     protected $fillable = [
-        'name', 'phone', 'email', 'password', 'introduction', 'avatar'
+        'name', 'phone', 'email', 'password', 'weixin_openid', 'weixin_unionid','introduction', 'avatar'
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,11 @@ Route::prefix('v1')->namespace('Api')
              // 用户注册
              Route::post('users', 'UsersController@store')
                  ->name('users.store');
+
+             // 第三方登录
+             Route::post('socials/{social_type}/authorizations','AuthorizationsController@socialStore')
+                 ->where('social_type','weixin')
+                 ->name('socials.authorizations.store');
          });
 
          Route::middleware('throttle:'. config('api.rate_limits.access'))->group(function(){
