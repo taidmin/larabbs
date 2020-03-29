e5c38ddad68a333212860126732e14abe88afdcd
vagrant <vagrant@homestead>
2020-03-29 08:32:10 +0000

    话题修改

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -29,9 +29,13 @@ class TopicsController extends Controller
         return new TopicResource($topic);
     }
 
-    public function update()
+    public function update(TopicRequest $request, Topic $topic)
     {
+        $this->authorize('update', $topic);
 
+        $topic->update($request->all());
+
+        return new TopicResource($topic);
     }
 
     public function destroy()
