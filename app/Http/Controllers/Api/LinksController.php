204f32af628e6f262dba5d60c9030147880a7c75
vagrant <vagrant@homestead>
2020-04-04 14:53:04 +0000

    推荐资源列表

---
+++ app/Http/Controllers/Api/LinksController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\LinkResource;
+use App\Models\Link;
+use Illuminate\Http\Request;
+
+class LinksController extends Controller
+{
+    public function index(Link $link)
+    {
+        $links = $link->getAllCached();
+
+        return LinkResource::collection($links);
+    }
+}
